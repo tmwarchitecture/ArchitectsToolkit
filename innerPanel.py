bc2ea1aa0e46b0f9e68df8dc6a71a340ec7856c7
tmwarchitecture <tmwarchitecture@gmail.com>
2017-05-30 09:32:10 +0800

    first
    makes a new surface from offsetting the edges.

--- innerPanel.py
+++ innerPanel.py
@@ -1,10 +1,9 @@
 import rhinoscriptsyntax as rs
 
-def innerFrame():
+def innerPanel():
     objs = rs.GetObjects("Select objects to add frame to", filter = 24, group = True,preselect = True)
     if objs is None:
         return
-    
     dist = rs.GetReal("Offset Distance")
     if dist is None:
         return
@@ -23,13 +22,15 @@ def innerFrame():
             edgeCrvs = rs.DuplicateEdgeCurves(srf)
             border = rs.JoinCurves(edgeCrvs, True)
             innerEdge = rs.OffsetCurveOnSurface(border, srf, dist)
-            rs.AddPlanarSrf([border, innerEdge])
+            #rs.SplitBrep(srf, innerEdge)
+            rs.AddPlanarSrf(innerEdge)
             rs.DeleteObject(innerEdge)
             rs.DeleteObject(border)
         else:
-            print "Surface is not planar"
+            print "A surface was not planar"
     rs.DeleteObjects(srfs)
     rs.EnableRedraw(True)
+    #rs.OffsetSurface(
 
 if __name__=="__main__":
-    innerFrame()
+    innerPanel()
