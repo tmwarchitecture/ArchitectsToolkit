6f993b3d56c8a3a38b5e3e0734060646ccf1ecb3
tmwarchitecture <tmwarchitecture@gmail.com>
2017-05-30 13:18:00 +0800

    Displays only 3dm files

--- components.py
+++ components.py
@@ -1,13 +1,32 @@
 import rhinoscriptsyntax as rs
 import os
 
-def getComponent():
+def addComponent():
     libPath = 'C:\\Users\\Tim\\Desktop\\temp\\library'
+    rhinoFiles = []
+    
     items = os.listdir(libPath)
-    choice = rs.ComboListBox(items)
+    
+    if items is None:
+        print "Library is empty or path is wrong"
+        return
+    
+    for item in items:
+        parts = item.split(".")
+        if len(parts)==2:
+            if parts[1]=="3dm":
+                rhinoFiles.append(parts[0])
+    
+    choice = rs.ComboListBox(rhinoFiles, "Select Components to add", "add Component")
     if choice is None:
         return
-    rs.Command('_-Import '+libPath+"\\"+choice+' Objects Enter')
-    rs.GetObjects()
+    choiceFile = choice+".3dm"
+    rs.EnableRedraw(False)
+    rs.Command('-_NoEcho _-Import '+libPath+"\\"+choiceFile+' Objects Enter -_SelLast Enter')
+    importedGeo = rs.GetObjects(preselect = True)
+    rs.ObjectLayer(importedGeo, rs.CurrentLayer())
+    rs.SelectObjects(importedGeo)
+    rs.EnableRedraw(True)
 
-getComponent()
+if __name__=="__main__":
+    addComponent()
