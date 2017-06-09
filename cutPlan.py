98e28f6d9a1daa5b6690943f68656d0ccea3206e
tmwarchitecture <tmwarchitecture@gmail.com>
2017-06-09 18:51:51 +0800

    Large update

--- cutPlan.py
+++ cutPlan.py
@@ -1,61 +1,75 @@
 import rhinoscriptsyntax as rs
-import ghpythonlib.components as ghcomp
-import scriptcontext as sc
-import Rhino as rh
+import scriptcontext
+from scriptcontext import doc
+import setLevels
 
 
-def unfilletContour(obj, pt):
+def cutAtPlan(level, floorNum):
+    planPlane = rs.AddPlaneSurface([-1000,-1000,level], 3000, 3000)
+    baseLayer = rs.AddLayer("60_PLANS")
+    newParent = rs.AddLayer("Level_"+floorNum, parent = baseLayer)
+    origLayer = rs.CurrentLayer()
+    shortName = rs.LayerName(origLayer, fullpath = False)
+    
+    #newChildsParent = rs.AddLayer( , parent = newParent)
+    newChild = rs.AddLayer(shortName, parent = newParent, color = rs.LayerColor(origLayer))
+    rs.CurrentLayer(newChild)
+    objs = rs.ObjectsByLayer(origLayer)
+    #if len(objs)<1:
+    #    skip = True
+    intersectCrvs = []
+    tempCrv = None
+    for obj in objs:
+        if rs.IsBrep(obj):
+            tempCrv = rs.IntersectBreps(obj, planPlane)
+        if tempCrv != None:
+            intersectCrvs.append(tempCrv)
+    
+    for crv in intersectCrvs:
+        if not None:
+            rs.ObjectLayer(crv, newChild)
+            #rs.SetUserText(crv, "PlanID", "PLAN")
+    
+    rs.DeleteObject(planPlane)
+    rs.CurrentLayer(origLayer)
+def deleteExistingPlans():
+    if rs.IsLayer("60_PLANS"):
+        #delete all sublayers
+        levelLayers = rs.LayerChildren("60_PLANS")
+        for levelLayer in levelLayers:
+            childLayers = rs.LayerChildren(levelLayer)
+            for childLayer in childLayers:
+                objects = rs.ObjectsByLayer(childLayer)
+                rs.DeleteObjects(objects)
+                rs.DeleteLayer(childLayer)
+            rs.DeleteLayer(levelLayer)
+    else:
+        return    
+def main():
+    heightRel2FFL = rs.GetReal("Cut level (relative to FFL):", number = 1)
+    levelsElev = setLevels.getFloorLevels()
     rs.EnableRedraw(False)
-    cir = rs.AddCircle(pt, 10000)
-    cirSrf = rs.AddPlanarSrf(cir)
+    deleteExistingPlans()
+    heights = []
     
-    curLay = rs.CurrentLayer()
-    childrenLay = rs.LayerChildren(curLay)
-    cutCrvs = []
-    contours = []
-    finalLines = []
-    finalLineSegs = []
-    #for lay in childrenLay:
-    #    objs = rs.ObjectsByLayer(lay)
-    #for obj in objs:
-    tempCrv = rs.IntersectBreps(obj, cirSrf)
-    if tempCrv is not None:
-        cutCrvs.append(tempCrv)
-    
-    for crv in cutCrvs:
-        contours.append(ghcomp.Explode(crv, True)[0])
-    
-    for contour in contours:
-        for i in range(0, len(contour)):
-            if rs.IsLine(contour[i]):
-                finalLines.append(sc.doc.Objects.AddCurve(contour[i]))
-                #print "A line!"
-    finalPts = []
-    for line in finalLines:
-        rs.ExtendCurveLength(line, 0, 2, 10)
-    for i in range(0,len(finalLines)-1):
-        tempPt = rs.CurveCurveIntersection(finalLines[i], finalLines[i+1])
-        finalPts.append(rs.AddPoint(rs.coerce3dpoint(tempPt[0][1])))
-    tempPt = rs.CurveCurveIntersection(finalLines[-1], finalLines[0])
-    finalPts.append(rs.AddPoint(rs.coerce3dpoint(tempPt[0][1])))
-    for i in range(0, len(finalPts)-1):
-        finalLineSegs.append(rs.AddLine(finalPts[i], finalPts[i+1]))
-    finalLineSegs.append(rs.AddLine(finalPts[-1], finalPts[0]))
-    lastCrv = rs.JoinCurves(finalLineSegs, True)
-    newSrf = rs.AddPlanarSrf(lastCrv)
-    sc.doc.Views.Redraw()
-    
-    rs.DeleteObjects(finalPts)
-    rs.DeleteObjects(finalLines)
-    rs.DeleteObject(tempCrv)
-    rs.DeleteObject(lastCrv)
-    rs.DeleteObjects(cutCrvs)
-    rs.DeleteObject(cir)
-    rs.DeleteObject(cirSrf)
+    for leveli in levelsElev:
+        heights.append(leveli+heightRel2FFL)
+    layers = rs.GetLayers()
+    if layers is None:
+        return
+    layersToCut = []
+    for layer in layers:
+        if rs.LayerVisible(layer):
+            layersToCut.append(layer)
+    for i in range(0,len(heights)):
+        for layer in layersToCut:
+            rs.CurrentLayer(layer)
+            if i<9:
+                levelNum = "0"+str(i+1)
+            else:
+                levelNum = str(i+1)
+            cutAtPlan(heights[i], levelNum)
     rs.EnableRedraw(True)
-    
-    rs.SelectObject(newSrf)
+    return None
 
-obj = rs.GetObject("Select objects")
-pt = rs.GetPoint()
-unfilletContour(obj, pt)
+main()
