7c0c0965cff7d821a3d2604c5c9a20485c1810b2
tmwarchitecture <tmwarchitecture@gmail.com>
2017-06-17 00:14:20 +0800

    Delete getProjectInfo.py

--- getProjectInfo.py
+++
@@ -1,9 +0,0 @@
-﻿import rhinoscriptsyntax as rs
-import Rhino
-import scriptcontext
-scriptcontext.doc = Rhino.RhinoDoc.ActiveDoc
-
-NAME = rs.GetDocumentData("SITE INFO", "NAME")
-GFA = rs.GetDocumentData("SITE INFO", "GFA")
-GREEN = rs.GetDocumentData("SITE INFO", "GREEN%")
-AREA = str(rs.Area(rs.ObjectsByLayer("SITE::BNDY")))
