faf29ef5a36ca8f68eac4842fa476b6ade99776d
tmwarchitecture <tmwarchitecture@gmail.com>
2017-10-10 22:31:41 +0800

    removed arcs

--- dimensionPline.py
+++ dimensionPline.py
@@ -8,21 +8,24 @@ def dimensionPline(pline, offsetDist):
     dimsLayer = rs.AddLayer("DIMS", parent = objParent, color = (100,100,100))
     segments = rs.ExplodeCurves(pline)
     for seg in segments:
-        endPt = rs.CurveEndPoint(seg)
-        stPt = rs.CurveStartPoint(seg)
-        tanVec = rs.VectorCreate(stPt, endPt)
-        offsetVec = rs.VectorRotate(tanVec, 90, [0,0,1])
-        offsetVec = rs.VectorUnitize(offsetVec)
-        offsetVec = rs.VectorScale(offsetVec, offsetDist)
-        offsetPt = rs.VectorAdd(stPt, offsetVec)
-        dim = rs.AddAlignedDimension(stPt, endPt, rs.coerce3dpoint(offsetPt))
-        rs.ObjectLayer(dim, dimsLayer)
-        rs.AddObjectToGroup(dim, dimGroup)
+        if rs.IsArc(seg):
+            pass
+        else:
+            endPt = rs.CurveEndPoint(seg)
+            stPt = rs.CurveStartPoint(seg)
+            tanVec = rs.VectorCreate(stPt, endPt)
+            offsetVec = rs.VectorRotate(tanVec, 90, [0,0,1])
+            offsetVec = rs.VectorUnitize(offsetVec)
+            offsetVec = rs.VectorScale(offsetVec, offsetDist)
+            offsetPt = rs.VectorAdd(stPt, offsetVec)
+            dim = rs.AddAlignedDimension(stPt, endPt, rs.coerce3dpoint(offsetPt))
+            rs.ObjectLayer(dim, dimsLayer)
+            rs.AddObjectToGroup(dim, dimGroup)
     rs.DeleteObjects(segments)
     return
 
 def main():
-    dist = 2
+    dist = 3
     objects = rs.GetObjects("Select Curves to Dimension", filter = 4, preselect = True)
     rs.EnableRedraw(False)
     if objects is None:
